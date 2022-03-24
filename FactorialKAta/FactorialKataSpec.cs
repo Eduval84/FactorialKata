9527d0f99bc60c038424c473c1afd3ff200443e9
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-24 23:25:16 +0000

    red test factorial of number one is one

--- FactorialKAta/FactorialKataSpec.cs
+++ FactorialKAta/FactorialKataSpec.cs
@@ -14,12 +14,18 @@ namespace FactorialKAta
             act.Should().Throw<ArgumentException>().WithMessage("Cant find the factorial of negative number");
         }
 
+        [Fact]
+        public void factorial_of_number_one_is_one()
+        {
+            var result=factorial.CalculateFactorial(1);
 
+            result.Should().Be(1);
+        }
     }
 
     public static class factorial
     {
-        public static void CalculateFactorial(int i)
+        public static int CalculateFactorial(int i)
         {
             throw new ArgumentException("Cant find the factorial of negative number");
         }
