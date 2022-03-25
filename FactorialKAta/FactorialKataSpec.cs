2fc2a77a790d8823237d908e0380b760e7b0c095
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-25 00:17:14 +0000

    add new calculate with recusivity

--- FactorialKAta/FactorialKataSpec.cs
+++ FactorialKAta/FactorialKataSpec.cs
@@ -11,7 +11,7 @@ namespace FactorialKAta
         [Fact]
         public void Cant_Find_The_Factorial_Of_Negative_Number()
         {
-            Action act = () => factorial.CalculateFactorial(-1);
+            Action act = () => factorial.Calculate_Factorial_withLoop(-1);
 
             act.Should().Throw<ArgumentException>().WithMessage("Cant find the factorial of negative number");
         }
@@ -19,7 +19,7 @@ namespace FactorialKAta
         [Fact]
         public void factorial_of_number_1_is_1()
         {
-            var result=factorial.CalculateFactorial(1);
+            var result = factorial.Calculate_Factorial_withLoop(1);
 
             result.Should().Be(1);
         }
@@ -27,35 +27,62 @@ namespace FactorialKAta
         [Fact]
         public void factorial_of_number_2_is_2()
         {
-            var result = factorial.CalculateFactorial(2);
+            var result = factorial.Calculate_Factorial_withLoop(2);
 
             result.Should().Be(2);
         }
 
-        [Fact]
-        public void factorial_of_number_3_is_6()
+        [Theory]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(5, 120)]
+        public void calculate_factorial_of_numbers(int input, int expected)
         {
-            var result = factorial.CalculateFactorial(3);
+            var result = factorial.Calculate_Factorial_withLoop(input);
 
-            result.Should().Be(6);
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(5, 120)]
+        public void calculate_factorial_of_numbers_with_recursivity(int input, int expected)
+        {
+            var result = factorial.Calculate_Factorial_With_Recursivity(input);
+
+            result.Should().Be(expected);
         }
     }
 
     public static class factorial
     {
-        public static int CalculateFactorial(int input)
+        public static int Calculate_Factorial_withLoop(int input)
         {
-            var factorial = 1;
-
             if (input <= 0)
                 throw new ArgumentException("Cant find the factorial of negative number");
 
-            for (int num = input; num > 1; num--)
+            var factorial = input;
+
+            for (int i = input - 1; i >= 1; i--)
             {
-                factorial = input * (input - 1);
+                factorial *= i;
             }
 
             return factorial;
+        }
+
+        public static int Calculate_Factorial_With_Recursivity(int input)
+        {
+            if (input <= 0)
+                throw new ArgumentException("Cant find the factorial of negative number");
+
+            if (input == 1)
+                return 1;
+            else
+            {
+                return input * Calculate_Factorial_With_Recursivity(input - 1);
+            }
         }
     }
 }
